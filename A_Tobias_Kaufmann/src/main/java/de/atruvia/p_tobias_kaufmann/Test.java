9fbfcba0ef9e20cb2224aa22015bf97dee0a06b0
saibot88 <tkaufmann.putzbrunn@gmail.com>
2022-07-20 11:36:14 +0200

    Test klasse angepasst

--- A_Tobias_Kaufmann/src/main/java/de/atruvia/p_tobias_kaufmann/Test.java
+++ A_Tobias_Kaufmann/src/main/java/de/atruvia/p_tobias_kaufmann/Test.java
@@ -3,10 +3,9 @@ package de.atruvia.p_tobias_kaufmann;
 public class Test {
 
     public static void main(String[] args) {
-        boolean sonneScheint = true;
-        while(sonneScheint == true){
-            System.out.println("Die Sonne Scheint");
-        }
+
+        System.out.println("Hello World");
+
     }
 
 }
